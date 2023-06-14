4a88cf1bcacd05687b913c8fb2cca5c82e99d3e1
unnine <eonninej@gmail.com>
2023-06-14 19:13:07 +0900

    [WIP] member controller

---
+++ src/main/java/com/market/member/api/CustomerController.java
@@ -0,0 +1,12 @@
+package com.market.member.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/member")
+public class MemberController {
+
+
+
+}
---
+++ src/main/java/com/market/member/domain/entity/Seller.java
@@ -0,0 +1,26 @@
+package com.customer.domain.entity;
+
+import com.common.domain.vo.Phone;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Email
+    String email;
+
+    @NotBlank
+    String name;
+
+    Phone phone;
+
+}
