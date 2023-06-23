99ef42e2166c3d4452c36a3b1012bddbbe57533a
eg <eonninej@gmail.com>
2023-06-23 19:19:37 +0900

    Add member - seller
    - seller list
    - seller detail
    - seller signUp
    - seller modify
    - seller withdraw

--- src/main/java/com/market/member/domain/entity/Seller.java
+++ src/main/java/com/market/member/domain/entity/Seller.java
@@ -1,11 +1,22 @@
-package com.customer.domain.entity;
+package com.market.member.domain.entity;
 
 import com.market.member.domain.vo.Phone;
+import com.market.member.dto.CustomerDto;
+import com.market.member.dto.SellerDto;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Seller {
 
     @Id
@@ -19,5 +30,11 @@ public class Seller {
     String name;
 
     Phone phone;
+
+    public void updateInfo(SellerDto to) {
+        this.email = to.getEmail();
+        this.name = to.getName();
+        this.phone = to.getPhone();
+    }
 
 }
