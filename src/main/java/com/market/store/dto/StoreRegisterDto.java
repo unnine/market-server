22bc317b735e2d09eda2e82104059f0f4cedb120
unnine <eonninej@gmail.com>
2023-06-25 17:50:12 +0900

    Add store api controller test code

--- src/main/java/com/market/store/dto/StoreRegisterDto.java
+++ src/main/java/com/market/store/dto/StoreRegisterDto.java
@@ -2,9 +2,11 @@ package com.market.store.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class StoreRegisterDto {
 
     @NotBlank
